988c550319a6fd7facb193a2875ac87385743a43
Ivan Bykov <renbrek@gmail.com>
2023-10-21 21:08:00 +0300

    add some infrastructure things

--- src/routes/saves.rs
+++ src/routes/saves.rs
@@ -1,19 +1,35 @@
-use axum::{Router, Json, routing::get};
-use serde_json::{Value, json};
-
-use crate::error::MyResult;
+// use crate::error::{MyError, MyResult};
+use axum::{
+    body::{boxed, Body, BoxBody},
+    http::{Request, Response, StatusCode},
+    routing::get,
+    Router,
+};
+use tower::ServiceExt;
+use tower_http::services::ServeDir;
 
 pub fn saves_routes() -> Router {
-    Router::new().route("/saves", get(api_saves_handler))
+    Router::new().nest_service("/saves", ServeDir::new("assets"))
 }
 
-async fn api_saves_handler() -> MyResult<Json<Value>>{
-    // Create the success body.
-	let body = Json(json!({
-		"result": {
-			"success": true
-		}
-	}));
+async fn api_saves_handler() -> Result<Response<BoxBody>, (StatusCode, String)> {
+    let res = get_static_file().await?;
+    println!("{:?}", res);
 
-	Ok(body)
+    if res.status() == StatusCode::NOT_FOUND {
+        println!("NOT_FOUND")
+    }
+    Ok(res)
+}
+
+async fn get_static_file() -> Result<Response<BoxBody>, (StatusCode, String)> {
+    let req = Request::builder().body(Body::empty()).unwrap();
+
+    match ServeDir::new("assets").oneshot(req).await {
+        Ok(res) => Ok(res.map(boxed)),
+        Err(err) => Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", err),
+        )),
+    }
 }
